81d87cc8e6d1d94999a7e3a2557b34d8fc29747e
Key-ice <50235175+Key-ice@users.noreply.github.com>
2023-11-12 07:52:44 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
